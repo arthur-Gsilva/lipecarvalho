7ec3edc9b5892bd6e5cd64748eeab4cb85df9ac7
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2025-05-19 09:29:27 -0300

    tirando imports inuteis

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
 import Link from "next/link";
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react";

efe196b3e384167b70037902c7f135f3874f0401
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2025-06-26 08:57:47 -0300

    ajustando imagens de certificado

--- src/components/cards/CertificateCard.tsx
+++ src/components/cards/CertificateCard.tsx
@@ -18,7 +18,7 @@ export const CertificateCard = ({ data }: Props) => {
         <Link href={data.link} target="_blank">
             <div className=" text-primary p-4 rounded-lg">
                 <div className="flex flex-col h-full items-center gap-2 ">
-                    <img src={data.image} alt="imagem do certificado" className="h-full w-auto rounded-xl border-3 border-primary"/>
+                    <img src={data.image} alt="imagem do certificado" className="h-auto w-full rounded-xl border-3 border-primary"/>
             
                     <h3 className="text-sm md:text-lg font-bold text-center uppercase">
                         {language === 'pt-br' ? data.portugueseTitle : data.englishTitle}
